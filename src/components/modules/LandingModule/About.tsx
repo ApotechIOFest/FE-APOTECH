942e0932af966844a4c51e5b6c3bfccd1c7fe7ee
Veivel <25278964+Veivel@users.noreply.github.com>
2023-05-01 00:24:34 +0700

    feat: landing hero, fix about section

--- src/components/modules/LandingModule/About.tsx
+++ src/components/modules/LandingModule/About.tsx
@@ -3,28 +3,30 @@ import Image from 'next/image'
 export default function About() {
   return (
     <>
-      <h1 className="lg:text-display-medium mb-10 md:text-display-small text-display-small font-bold leading-none text-blue-darkest">
-        About Apotech
-      </h1>
-      <div className="flex flex-col lg:flex-row gap-10 justify-center items-center mb-10">
+      <div className="relative flex flex-col md:flex-row gap-10 justify-center items-center mb-10 min-h-[40vw]">
         <Image
-          height={54}
-          width={54}
-          className="w-[1800px] h-72"
+          height={320}
+          width={320}
+          className="relative lg:-mx-32 md:-mx-16 -mx-3 2xl:w-[30vw] 2xl:h-[30vw] xl:w-[35vw] xl:h-[35vw] md:w-[40vw] md:h-[40vw] w-0 h-0"
           src="/assets/images/about-image.png"
           alt=""
         />
-        <a
-          href="#2"
-          className="block relative text-center lg:text-left group-hover:text-purple-500 transition-colors duration-200"
-        >
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a type specimen book. It has survived not
-          only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </a>
+        <div className="lg:pl-32 md:pl-16 pl-3">
+          <h1 className="text-center md:text-left relative lg:text-display-medium mb-10 md:text-display-small text-display-small font-bold leading-none text-blue-darkest">
+            About Apotech
+          </h1>
+          <a
+            href="#2"
+            className="block relative text-center md:text-left group-hover:text-purple-500 transition-colors duration-200 2xl:pr-[30vw] lg:pr-32 md:pr-8 pr-3"
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry&apos;s standard dummy
+            text ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has survived
+            not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged.
+          </a>
+        </div>
       </div>
     </>
   )
--- src/components/modules/LandingModule/Hero.tsx
+++ src/components/modules/LandingModule/Hero.tsx
@@ -1,3 +1,99 @@
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+const LETTER_INTERVAL_IN_MILLISECONDS = 65
+const KEYWORD_INTERVAL_IN_MILLISECONDS = 1500
+const KEYWORDS_LIST: string[] = [
+  'pharmacy',
+  'website',
+  'medicine store',
+  'apothecary',
+  'service',
+]
+
 export default function Hero() {
-  return <></>
+  const [keywordIndex, setKeywordIndex] = useState<number>(-1)
+  const [currentKeyword, setCurrentKeyword] = useState<string>('pharmacy')
+
+  // functional, recursive logic for keyword animation
+
+  function switchKeyword() {
+    setKeywordIndex((prev) => (prev + 1) % KEYWORDS_LIST.length)
+  }
+
+  function writeKeyword(keyword: string) {
+    setTimeout(() => {
+      setCurrentKeyword((prev) => prev.concat(keyword.slice(0, 1)))
+      if (keyword.slice(1)) {
+        writeKeyword(keyword.slice(1))
+      } else {
+        switchKeyword()
+      }
+    }, LETTER_INTERVAL_IN_MILLISECONDS)
+  }
+
+  function sliceKeyword(len: number) {
+    setTimeout(() => {
+      setCurrentKeyword((prev) => prev.slice(0, prev.length - 1))
+      if (len > 1) {
+        sliceKeyword(len - 1)
+      }
+    }, LETTER_INTERVAL_IN_MILLISECONDS)
+  }
+
+  useEffect(() => {
+    if (keywordIndex === -1) {
+      setKeywordIndex(0)
+      return
+    }
+
+    const len = currentKeyword.length
+    if (len >= 1) {
+      setTimeout(() => {
+        sliceKeyword(len)
+      }, KEYWORD_INTERVAL_IN_MILLISECONDS)
+    } else {
+      writeKeyword(KEYWORDS_LIST[keywordIndex])
+    }
+  }, [keywordIndex])
+
+  useEffect(() => {
+    if (!(currentKeyword.length >= 1)) {
+      switchKeyword()
+    }
+  }, [currentKeyword])
+
+  return (
+    <>
+      <div className="relative flex flex-col md:flex-row justify-center items-center mb-10 min-h-[38vw] w-full">
+        {/* left */}
+        <div
+          className="flex flex-row relative mb-10 font-bold leading-none
+        text-center md:text-left lg:text-display-medium text-display-small
+        md:w-[50%] md:max-w-[55%] w-[100%] h-[80vw] md:h-fit mx-auto md:mx-0 md:mr-auto md:px-0 px-[10vw] sm:px-[8vw]"
+        >
+          <h1 className="text-black mr-3 2xl:pl-[15vw] md:my-0 my-auto">
+            Apotech is more than just a{' '}
+            <p className="inline text-blue-darkest font-productSansBold font-black underlinable w-fit">
+              {currentKeyword}
+            </p>
+            <p className="inline">.</p>
+          </h1>
+        </div>
+
+        {/* <div className="bg-red-300 ml-auto relative"> */}
+        <Image
+          height={320}
+          width={320}
+          className="
+            absolute right-0 top-0 lg:-mr-32 md:-mr-16 -mr-3 lg:-mt-20 md:-mt-16 -mt-12
+            ml-auto md:w-[40vw] md:h-[40vw] w-0 h-0
+            "
+          src="/assets/images/hero-image.png"
+          alt=""
+        />
+        {/* </div> */}
+      </div>
+    </>
+  )
 }
