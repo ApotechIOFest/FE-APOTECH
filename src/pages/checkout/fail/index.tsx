b3198c058aab0ba5b911841aa2609eb22082efaf
Veivel <25278964+Veivel@users.noreply.github.com>
2023-05-02 21:12:50 +0700

    feat: pages for checkout

---
+++ src/pages/checkout/fail/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import { CheckoutFailModule } from 'src/components/modules/CheckoutModule/CheckoutFail';
+
+const CheckoutFail: NextPage = () => <CheckoutFailModule />
+
+export default CheckoutFail
---
+++ src/pages/checkout/pending/[id].tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import { CheckoutPendingModule } from 'src/components/modules/CheckoutModule/CheckoutPending'
+
+const Checkout: NextPage = () => <CheckoutPendingModule />
+
+export default Checkout
