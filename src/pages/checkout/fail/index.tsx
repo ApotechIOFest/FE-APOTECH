6f9b092613c52182605c742dfba01bd5e7ec82e9
michaelsinanta <michaelchristlambert@gmail.com>
2023-05-02 22:21:24 +0700

    fix: forum

--- src/pages/checkout/fail/index.tsx
+++ src/pages/checkout/fail/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { NextPage } from 'next'
-import { CheckoutFailModule } from 'src/components/modules/CheckoutModule/CheckoutFail';
+import { CheckoutFailModule } from 'src/components/modules/CheckoutModule/CheckoutFail'
 
 const CheckoutFail: NextPage = () => <CheckoutFailModule />
 
